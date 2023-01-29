3acecfe93b6354219e32878524913c458152170e
danielmdavis <danis1911@gmail.com>
2023-01-29 15:05:51 -0500

    hold down a to set random color on release

--- hue.py
+++ hue.py
@@ -1,56 +1,83 @@
 import phue
 from phue import Bridge
+import random
 from pynput.keyboard import Listener
 
-b = phue.Bridge('192.168.2.100')
-b.connect()
+bridge = phue.Bridge('192.168.2.100')
+bridge.connect()
 
-b.get_api()
+bridge.get_api()
 
 off = {'transitiontime' : 0, 'on' : False}
 on = {'transitiontime' : 0, 'bri': 255, 'on' : True}
+onColor = {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), 1]}
+
+aHeld = False
+
+
 
 def on_press(key):
+    if key.char == 'a':
+        global aHeld
+        aHeld = True
+
     # floor lamp
     if key.char == '7':
-        b.set_light(6, off)
+        bridge.set_light(6, off)
     if key.char == '8':
-        b.set_light(5, off)
+        bridge.set_light(5, off)
     if key.char == '9':
-        b.set_light(9, off)
-
+        bridge.set_light(9, off)
     # low lamp
     if key.char == '4':
-        b.set_light(7, off)
-
+        bridge.set_light(7, off)
     # under bed
     if key.char == '5':
-        b.set_light(8, off)
-
+        bridge.set_light(8, off)
     # behind monitor
     if key.char == '6':
-        b.set_light(10, off)
+        bridge.set_light(10, off)
 
 def on_release(key):
+    if key.char == 'a':
+        global aHeld
+        aHeld = False
+
     # floor lamp
     if key.char == '7':
-        b.set_light(6, on)
+        if aHeld == True:
+            bridge.set_light(6, {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+        else:
+            bridge.set_light(6, on)
     if key.char == '8':
-        b.set_light(5, on)
+        if aHeld == True:
+            bridge.set_light(5, {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+        else:
+            bridge.set_light(5, on)
     if key.char == '9':
-        b.set_light(9, on)
-
+        if aHeld == True:
+            bridge.set_light(9, {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+        else:
+            bridge.set_light(9, on)
     # low lamp
     if key.char == '4':
-        b.set_light(7, on)
-
+        if aHeld == True:
+            bridge.set_light(7, {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+        else:
+            bridge.set_light(7, on)
     # under bed
     if key.char == '5':
-        b.set_light(8, on)
-
+        if aHeld == True:
+            bridge.set_light(8, {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+        else:
+            bridge.set_light(8, on)
     # behind monitor
     if key.char == '6':
-        b.set_light(10, on)
+        if aHeld == True:
+            bridge.set_light(10, {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+        else:
+            bridge.set_light(10, on)
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
