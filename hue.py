e40bf639ea16667f55e2176ca2b799cd0227325e
danielmdavis <danis1911@gmail.com>
2023-01-29 13:06:46 -0500

    each num pad input controls a seperate light

--- hue.py
+++ hue.py
@@ -11,11 +11,46 @@ off = {'transitiontime' : 0, 'on' : False}
 on = {'transitiontime' : 0, 'bri': 255, 'on' : True}
 
 def on_press(key):
-    # print('foo')
-    b.set_light(10, off)
+    # floor lamp
+    if key.char == '7':
+        b.set_light(6, off)
+    if key.char == '8':
+        b.set_light(5, off)
+    if key.char == '9':
+        b.set_light(9, off)
+
+    # low lamp
+    if key.char == '4':
+        b.set_light(7, off)
+
+    # under bed
+    if key.char == '5':
+        b.set_light(8, off)
+
+    # behind monitor
+    if key.char == '6':
+        b.set_light(10, off)
 
 def on_release(key):
-    b.set_light(10, on)
+    # floor lamp
+    if key.char == '7':
+        b.set_light(6, on)
+    if key.char == '8':
+        b.set_light(5, on)
+    if key.char == '9':
+        b.set_light(9, on)
+
+    # low lamp
+    if key.char == '4':
+        b.set_light(7, on)
+
+    # under bed
+    if key.char == '5':
+        b.set_light(8, on)
+
+    # behind monitor
+    if key.char == '6':
+        b.set_light(10, on)
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
