72e48ed4ec0b5360637aa034604167489853a972
danielmdavis <danis1911@gmail.com>
2023-01-29 12:29:50 -0500

    all keys control one light, depress off release on, pythonically

--- hue.py
+++ hue.py
@@ -1,9 +1,21 @@
 import phue
 from phue import Bridge
+from pynput.keyboard import Listener
 
 b = phue.Bridge('192.168.2.100')
 b.connect()
 
 b.get_api()
 
-b.set_light(6, 'off', True)
+off = {'transitiontime' : 0, 'on' : False}
+on = {'transitiontime' : 0, 'bri': 255, 'on' : True}
+
+def on_press(key):
+    # print('foo')
+    b.set_light(10, off)
+
+def on_release(key):
+    b.set_light(10, on)
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
