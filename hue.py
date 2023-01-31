46510fd1836dee035c31ceba43f37cd33cda596a
danielmdavis <danis1911@gmail.com>
2023-01-31 15:01:32 -0500

    DRYer still, fully dry. scaffolding more new colors

--- hue.py
+++ hue.py
@@ -21,27 +21,50 @@ on_red = {'transitiontime' : 0, 'bri': 255, 'on' : True, 'xy': converter.rgb_to_
 on_green = {'transitiontime' : 0, 'bri': 255, 'on' : True, 'xy': converter.rgb_to_xy(0, 255, 0)}
 on_blue = {'transitiontime' : 0, 'bri': 255, 'on' : True, 'xy': converter.rgb_to_xy(0, 0, 255)}
 
+# color setters: ROYGBIV and other
+A_held, O_held, E_held, U_held, I_held, D_held, H_held = False, False, False, False, False, False, False
 Q_held = False
 
-A_held = False
-O_held = False
-E_held = False
+global light_id
+light_id = {
+    '7': 6, # lamp tree top
+    '8': 5, # lamp tree mid
+    '9': 9, # lamp tree bottom
+    '4': 7, # low lamp
+    '5': 8, # under bed
+    '6': 10 # behind monitor
+}
+
+def parse_light_press(key):
+    bridge.set_light(light_id[key.char], off)
+
+def parse_light_release(key):
+    if A_held == True:
+        bridge.set_light(light_id[key.char], on_red)
+    elif O_held == True:
+        bridge.set_light(light_id[key.char], on_green)
+    elif E_held == True:
+        bridge.set_light(light_id[key.char], on_blue)
+    elif Q_held == True:
+        bridge.set_light(light_id[key.char], {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
+    else:
+        bridge.set_light(light_id[key.char], on)
+
 
 
 def parse_color_press(key):
-    if key.char == 'a':
-        global A_held
-        A_held = True
-    if key.char == 'o':
-        global O_held
-        O_held = True
-    if key.char == 'e':
-        global E_held
-        E_held = True
+    global A_held, O_held, E_held, U_held, I_held, D_held, H_held
+    global Q_held
 
-    if key.char == 'q':
-        global Q_held
-        Q_held = True
+    if key.char == 'a': A_held = True
+    if key.char == 'o': O_held = True
+    if key.char == 'e': E_held = True
+    if key.char == 'u': U_held = True
+    if key.char == 'i': I_held = True
+    if key.char == 'd': D_held = True
+    if key.char == 'h': H_held = True
+
+    if key.char == 'q': Q_held = True
 
 def parse_color_release(key):
     if key.char == 'a':
@@ -58,48 +81,16 @@ def parse_color_release(key):
         global Q_held
         Q_held = False        
 
-def parse_light_release(key):
 
-    light_id = {
-        '7': 6, # lamp tree top
-        '8': 5, # lamp tree mid
-        '9': 9, # lamp tree bottom
-        '4': 7, # low lamp
-        '5': 8, # under bed
-        '6': 10 # behind monitor
-    }
 
-    if A_held == True:
-        bridge.set_light(light_id[key.char], on_red)
-    elif O_held == True:
-        bridge.set_light(light_id[key.char], on_green)
-    elif E_held == True:
-        bridge.set_light(light_id[key.char], on_blue)
-    elif Q_held == True:
-        bridge.set_light(light_id[key.char], {'transitiontime' : 0, 'on' : True, 'bri': 255, 'xy': [random.random(), random.random()]})
-    else:
-        bridge.set_light(light_id[key.char], on)
+
 
 def on_press(key):
 
     parse_color_press(key)
+    parse_light_press(key)
 
-    # floor lamp, descending
-    if key.char == '7':
-        bridge.set_light(6, off)
-    if key.char == '8':
-        bridge.set_light(5, off)
-    if key.char == '9':
-        bridge.set_light(9, off)
-    # low lamp
-    if key.char == '4':
-        bridge.set_light(7, off)
-    # under bed
-    if key.char == '5':
-        bridge.set_light(8, off)
-    # behind monitor
-    if key.char == '6':
-        bridge.set_light(10, off)
+
 
 def on_release(key):
     
